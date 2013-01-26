676494ea35c1fc0a6fed6e6cf9e73cc9d296fcb1
John Anderson <anderson.johnd@gmail.com>
2013-01-26 09:35:51 -0800

    How to Begin copy changes.

--- app/views/pages/how-to-begin.html.php
+++ app/views/pages/how-to-begin.html.php
@@ -2,30 +2,30 @@
 
 <h1>How To Begin</h1>
 
-<h2>Step 1:</h2>
-<?= $this->html->image('engineering_for_good.png', array('class' => 'accent right')); ?>
-<p>Contact Troy Holmberg of US Synthetic Engineering Good at tholmberg@ussynthetic.com, or Rinda Hayes of Kenya Keys at rinda@kenyakeys.org.</p>
-<p><?= $this->html->link('Download our Introduction Flyer', '/pdf/gc_flyer_2011.pdf'); ?></p>
+<h2>STEP 1: PLAN</h2>
 
-<h2>Step 2:</h2>
-<p>Set up a time to discuss your interests, needs, and choose a project. Typically involvement works best coming from the student council, a club faculty advisor, or Parent Teacher Organizations. For example: Do you want to educate your group and give them an appreciation for all they have or are you simply looking for a service project? Do you want to connect with a sister school and find ways to exchange and communicate?</p>
+<p><strong>Contact Dorothy Winters <a href="mailto:Dorothy@kenyakeys.org">mailto:Dorothy@kenyakeys.org</a> or Rinda Hayes <a href="mailto:rinda@kenyakeys.org">rinda@kenyakeys.org.</a></strong></p>
 
-<h2><span>Projects:</span></h2>
-<?= $this->html->image('love.png', array('class' => 'accent left')); ?>
-<p>Provide school uniforms, build desks, donate books and school supplies, build a school library, add a classroom or latrine facilities to a school in need.</p>
-<p>Pick a project that your students will enjoy supporting according to the funds you believe you can raise. Pick a project and sister school to support, and design a service activity to raise funds for your sister school. </p>
-<p>Set a goal and a time-frame for participation. Usually shorter, focused campaigns work best.</p>
+<p><strong>Set up a time with Rinda or Dorothy to discuss your objectives, interests and needs.</strong> Choose a sister school and corresponding project.  Each school is different, both in the U.S. and in Kenya.  We want to help you plan a Global Classroom experience that is right for your school or classroom.  Projects can range from  raising money to build desks to building classrooms and latrines.  Or perhaps your school/class would like to focus on books and library assistance.  Nothing is more rewarding than getting books in the hands of children who have never had them!</a>
 
-<h2>Step 3:</h2>
-<p>Schedule time to have us educate your youth group in a 45-minute presentation to introduce the program. We will teach them about life in Kenya and have a model Kenyan village that students can experience along with video and pictures from Kenya. We will show your students pictures of their sister school and motivate them to take action to make a difference. The student council or youth leader can pitch the service project scope and timeframe to the students.</p>
+<p>You could also use our <strong>Strides for Education</strong> program as part of your plan.  It is a fun, easy way to get students involved in helping locally and globally.</p>
 
-<h2>Step 4:</h2>
-<p>Do your service projects with the group. Again short, focused campaigns and projects will be more successful.</p>
+<p><strong>Plan activities that will raise funds for your sister school. Make it fun!</strong>   Initiating this plan through the student council and their advisor usually works best.  PTA involvement call also be most helpful.  Or maybe you just have a class that wants to be involved.  Your involvement can be as large or small as you wish.</p>
 
-<h2>Step 5:</h2>
-<p>Goal achievement celebration/assembly (optional). Report on the success of the service and congratulate the students on their success. Memorialize if you wish with pictures/photos of the students and the sister school in a display at school.</p>
+<h2>STEP 2: LEARN</h2>
 
-<h2>Step 6:</h2>
-<p>Follow-up/Share with students how their efforts impacted their sister school. Continued contact between schools may include: making a short video, sending letters, photos, exchanging murals painted by the students, exchanging hand-made bracelets, sharing via a common Facebook page, etc.</p>
+<p><strong>Schedule a time to have Kenya Keys educate your group with a fun, interactive presentation about life  in the African bush.</strong>  This can be a whole school assembly or a grade presentation.  Your students will realize how much they have to be grateful for when they see what Kenyan students are up against.  A model Kenyan village, videos and pictures make it all real.</p>
+
+<p>We will also show your students pictures of their sister school and motivate them to take action to make a difference.</p>
+
+<h2>STEP 3: TAKE ACTION</h2>
+
+<p><strong>Work together to raise funds for your sister school’s needs.</strong>  There are lots of ways to get the students enthused.   For fundraising ideas, check out our  Fun Fundraising Ideas.</p>
+
+<p><strong>Celebrate the success of the service/fundraising and congratulate the students.</strong>  Consider having an achievement celebration or assembly (optional).  Memorialize your success with pictures in a display at school.  Kenya Keys offers support for all of this.</p>
+
+<h2>WATCH IT UNFOLD</h2>
+
+<p>One of the most rewarding and unique aspects of The Global Classroom is the follow up.  <strong>We will return and show the students how their efforts have impacted their sister school.</strong>  They will get to see photos of their finished projects.  Continuing and building a relationship between the two schools is the best part of all! </p>
 
 <p class="highlight">100% of all donations go to Kenyan Students!</p>
